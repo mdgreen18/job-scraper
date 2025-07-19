1bc9478ddc8e804ad80e413103cfc608d6e179ec
Matthew Greenwood <matthewdgreen18@protonmail.com>
2025-07-19 16:21:46 -0400

    Created the save function and showcases the csv file it created

--- scraper.py
+++ scraper.py
@@ -1,4 +1,5 @@
 import requests
+import csv
 from bs4 import BeautifulSoup
 
 def fetch_jobs(url):
@@ -18,6 +19,24 @@ def display_jobs(jobs):
     for i, (title, company) in enumerate(jobs, 1):
         print(f"{i}. {title} at {company}")
 
+def save_file(jobs):
+    # Makes field names for csv file
+    fields = ['Title', 'Company']
+
+    # Create a name for csv file
+    filename = "job_list.csv"
+
+    with open(filename, 'w') as csvfile:
+        # Create a writer object
+        csvwriter = csv.writer(csvfile)
+
+        # Write the fields sections
+        csvwriter.writerow(fields)
+
+        # Enter the scraped data
+        csvwriter.writerows(jobs)
+    
+
 if __name__ == "__main__":
     url = "https://realpython.github.io/fake-jobs/"
     job_list = fetch_jobs(url)
@@ -25,7 +44,7 @@ if __name__ == "__main__":
     while True:
         print('\nPlease select an option:\n')
         print('1. Display jobs')
-        print('2. Send job list to .csv file')
+        print('2. Save job list to .csv file')
         print('3. Quit')
 
         choice = int(input('\nEnter your choice: '))
@@ -33,7 +52,7 @@ if __name__ == "__main__":
         if choice == 1:
             display_jobs(job_list)
         elif choice == 2:
-            break #update with new function in th future
+            save_file(job_list)
         elif choice == 3:
             break
     print('Happy job hunting!')
